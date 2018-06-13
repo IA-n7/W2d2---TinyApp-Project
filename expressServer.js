31644673951a64055194e9cf8ff3d5b1b206c1b6
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-13 16:01:21 +0000

    Added delete and update functions/inputs

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,9 +13,9 @@ var urlDatabase = {
 };
 
 function generateRandomString() {
-  var randomString = "";
-  var allPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < 5; i++)
+  let randomString = "";
+  let allPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 5; i++)
     randomString += allPossibleCharacters.charAt(Math.floor(Math.random() * allPossibleCharacters.length));
   return randomString;
 }
@@ -30,7 +30,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//
+//GGeneration Page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -45,18 +45,41 @@ app.get("/urls/:id", (req, res) => {
 
 //Home page
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.end("OK");
+});
+
+//Redirect from /u
+app.get("/u/:shortURL", (req, res) => {
+  let short = req.params["shortURL"];
+  res.redirect(urlDatabase[short]);
 });
 
 //POSTing submitted URLs
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  var long = req.body["longURL"];
-  var urlID = generateRandomString();
+  let long = req.body["longURL"];
+  let urlID = generateRandomString();
   res.redirect(`/urls/${urlID}`);
   urlDatabase[urlID] = long;
 });
 
+//POSTing delete URL
+app.post("/urls/:id/delete", (req, res) => {
+  let short = req.params.id;
+  delete urlDatabase[short];
+  res.redirect(`/urls`);
+});
+
+//POSTing an updated longURL
+app.post("/urls/:id/update", (req, res) => {
+  console.log(req.params);
+  console.log(req.body["longURL"]);
+  let long = req.body["longURL"];
+  let short = req.params["id"];
+  urlDatabase[short] = long;
+  res.redirect(`/urls/`);
+});
+
+//Listening
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
